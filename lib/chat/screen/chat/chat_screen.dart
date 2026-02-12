8b82aacc58878aa090b389dbd5c5df5642d340c7
mine0059 <onojefemuemine@gmail.com>
2026-02-12 20:26:30 +0100

    update: chat screen with scaffold

--- lib/chat/screen/chat/chat_screen.dart
+++ lib/chat/screen/chat/chat_screen.dart
@@ -1,6 +1,8 @@
 import 'package:chat_app/chat/model/user_model.dart';
 import 'package:flutter/material.dart';
 
+import 'widgets/user_chat_profile.dart';
+
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key, required this.chatId, required this.otherUser});
 
@@ -14,6 +16,14 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        forceMaterialTransparency: true,
+        backgroundColor: Colors.white,
+        title: UserChatProfile(widget: widget),
+        titleSpacing: 0,
+      ),
+    );
   }
 }
